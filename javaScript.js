3d4d226326dbf77cebb9614b9321d58b1e2be5fa
Aasland <frederikaasland21@gmail.com>
2024-02-08 19:59:33 +0100

    Ferdig Nightmode

--- javaScript.js
+++ javaScript.js
@@ -1,8 +1,26 @@
+// Night/Light-mode start
+
+icon = document.getElementById("moonIcon");
+
+icon.onclick = function () {
+  document.body.classList.toggle("darkTheme");
+  if (icon.classList.contains("darkTheme")) {
+    icon.src = "Bilder/sun.png";
+  } else {
+    icon.src = "Bilder/moon.png";
+  }
+};
+
+// Night/Light-mode end
+
+// Login start
+
 const wrapper = document.querySelector(".wrapper");
 const loginLink = document.querySelector(".login-link");
 const registerLink = document.querySelector(".register-link");
 const btnPopup = document.querySelector(".btnLogin-popup");
 const iconClose = document.querySelector(".icon-close");
+const loginClose = document.getElementsByClassName("btn");
 
 registerLink.addEventListener("click", () => {
   wrapper.classList.add("active");
@@ -19,3 +37,9 @@ btnPopup.addEventListener("click", () => {
 iconClose.addEventListener("click", () => {
   wrapper.classList.remove("active-popup");
 });
+
+loginClose.onclick = function () {
+  wrapper.classList.remove("active-popup");
+};
+
+// Login end
