cf9a8d827ba1895ddc2a9275c08cee8e76029053
Aasland <frederikaasland21@gmail.com>
2024-03-01 10:47:27 +0100

    ljsn

--- javaScript.js
+++ javaScript.js
@@ -13,32 +13,6 @@ icon.onclick = function () {
 
 // Night/Light-mode end
 
-// Login start
-
-const wrapper = document.querySelector(".wrapper");
-const loginLink = document.querySelector(".login-link");
-const registerLink = document.querySelector(".register-link");
-const btnPopup = document.querySelector(".btnLogin-popup");
-const iconClose = document.querySelector(".icon-close");
-
-registerLink.addEventListener("click", () => {
-  wrapper.classList.add("active");
-});
-
-loginLink.addEventListener("click", () => {
-  wrapper.classList.remove("active");
-});
-
-btnPopup.addEventListener("click", () => {
-  wrapper.classList.add("active-popup");
-});
-
-iconClose.addEventListener("click", () => {
-  wrapper.classList.remove("active-popup");
-});
-
-// Login end
-
 // GDPR start
 
 const gdpr = document.querySelector(".gdpr");
@@ -102,59 +76,57 @@ const endreH2Tilpass = function () {
 //Instagram
 
 // automatisering av bilde karusell
-var counter = 1
+var counter = 1;
 setInterval(function () {
-    document.getElementById('radio' + counter).checked = true
-    counter++
-    if (counter > 3) {
-        counter = 1
-    }
-}, 10000)
+  document.getElementById("radio" + counter).checked = true;
+  counter++;
+  if (counter > 3) {
+    counter = 1;
+  }
+}, 10000);
 
 // kommentarfelt
 
-"use strict";
+("use strict");
 
 const userId = {
-    name: null,
-    identity: null,
-    image: null,
-    message: null,
-    date: null
-}
+  name: null,
+  identity: null,
+  image: null,
+  message: null,
+  date: null,
+};
 
-const userComment = document.querySelector(".usercomment")
-const publishBtn = document.querySelector("#publish")
-const comments = document.querySelector(".comments")
-const userName = document.querySelector(".user")
+const userComment = document.querySelector(".usercomment");
+const publishBtn = document.querySelector("#publish");
+const comments = document.querySelector(".comments");
+const userName = document.querySelector(".user");
 
-userComment.addEventListener("input", e => {
-    if (!userComment.value) {
-        publishBtn.setAttribute("disabled", "disabled")
-        publishBtn.classList.remove("abled")
-    } else {
-        publishBtn.removeAttribute("disabled")
-        publishBtn.classList.add("abled")
-    }
-})
-
+userComment.addEventListener("input", (e) => {
+  if (!userComment.value) {
+    publishBtn.setAttribute("disabled", "disabled");
+    publishBtn.classList.remove("abled");
+  } else {
+    publishBtn.removeAttribute("disabled");
+    publishBtn.classList.add("abled");
+  }
+});
 
 function addPost() {
-    console.log("Knappen fungerer")
-    //if (userComment.value) return
-    userId.name = userName.value
-    if (userId.name === "Anonymous") {
-        userId.identity = false
-        userId.image = "anonymous.png"
-    } else {
-        userId.identity = true
-        userId.image = "user.png"
-    }
+  console.log("Knappen fungerer");
+  //if (userComment.value) return
+  userId.name = userName.value;
+  if (userId.name === "Anonymous") {
+    userId.identity = false;
+    userId.image = "anonymous.png";
+  } else {
+    userId.identity = true;
+    userId.image = "user.png";
+  }
 
-    userId.message = userComment.value
-    userId.date = new Date().toLocaleString()
-    let published =
-        `<div class="parents"> 
+  userId.message = userComment.value;
+  userId.date = new Date().toLocaleString();
+  let published = `<div class="parents"> 
             
             <div> 
                 <h1>${userId.name}</h1>
@@ -162,19 +134,15 @@ function addPost() {
                 <div class="engagements"> <img src="bilder/like.png"><img src="bilder/share.png"></div> 
                 <span class="date">${userId.date}</span>
             </div> 
-        </div>`
+        </div>`;
 
-    comments.innerHTML += published
-    userComment.value = ""
+  comments.innerHTML += published;
+  userComment.value = "";
 
-    let commentsNum = document.querySelectorAll(".parents").length
-    document.getElementById("comment").textContent = commentsNum
-
+  let commentsNum = document.querySelectorAll(".parents").length;
+  document.getElementById("comment").textContent = commentsNum;
 }
 
-//<img src="${userId.image}"> 
+//<img src="${userId.image}">
 
-publishBtn.addEventListener("click", addPost)
-
-
-
+publishBtn.addEventListener("click", addPost);
