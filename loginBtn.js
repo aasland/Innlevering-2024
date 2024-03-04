b9e2fb96194d2229222f39d9a09d820576c03317
Aasland <frederikaasland21@gmail.com>
2024-03-04 09:43:54 +0100

    login color

--- loginBtn.js
+++ loginBtn.js
@@ -5,6 +5,7 @@ const loginLink = document.querySelector(".login-link");
 const registerLink = document.querySelector(".register-link");
 const btnPopup = document.querySelector(".btnLogin-popup");
 const iconClose = document.querySelector(".icon-close");
+const loginBtn = document.querySelector(".btn");
 
 registerLink.addEventListener("click", () => {
   wrapper.classList.add("active");
@@ -22,12 +23,17 @@ iconClose.addEventListener("click", () => {
   wrapper.classList.remove("active-popup");
 });
 
+loginBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  wrapper.classList.remove("active-popup");
+});
+
 // Login end
 
 // Change icon start
 
-if (loginLink.onclick) {
-  btnPopup = `<ion-icon name="log-out-outline"></ion-icon> log out`;
-}
+switchLoginString = function () {
+  btnPopup.innerHTML = `<ion-icon name="log-out"></ion-icon> Log out`;
+};
 
 // Change icon end
