3499145c40e05c2ee288c9a1d4ceaa0dbfbc8cc8
HannaJohns <hanna.tomter.johns@gmail.com>
2024-03-08 10:47:13 +0100

    modal update

--- insta-modal.js
+++ insta-modal.js
@@ -2,28 +2,28 @@ const openModalImages = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
 
-openModalButtons.forEach(image => {
-    image.addEventListner('click', () => {
+openModalImages.forEach(image => {
+    image.addEventListener('click', () => {
         const modal = document.querySelector(image.dataset.modalTarget)
         openModal(modal)
     })
 })
 
 closeModalButtons.forEach(button => {
-    button.addEventListner('click', () => {
+    button.addEventListener('click', () => {
         const modal = button.closest('.modal')
         closeModal(modal)
     })
 })
 
 function openModal(modal) {
-    if(modal == 0) return
+    if (!modal) return
     modal.classList.add('active')
     overlay.classList.add('active')
 }
 
 function closeModal(modal) {
-    if(modal == 0) return
+    if (!modal) return
     modal.classList.remove('active')
     overlay.classList.remove('active')
 }
