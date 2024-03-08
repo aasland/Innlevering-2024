6a65fd0be524b065842f3c0de6304397fdd6e395
HannaJohns <hanna.tomter.johns@gmail.com>
2024-03-08 10:41:10 +0100

    Update insta-modal.js

--- insta-modal.js
+++ insta-modal.js
@@ -1,10 +1,10 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const openModalImages = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
 
-openModalButtons.forEach(button => {
-    button.addEventListner('click', () => {
-        const modal = document.querySelector(button.dataset.modalTarget)
+openModalButtons.forEach(image => {
+    image.addEventListner('click', () => {
+        const modal = document.querySelector(image.dataset.modalTarget)
         openModal(modal)
     })
 })
