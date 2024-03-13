713117a0116dff9e76ac6e15d832aef34e5354f0
SOFIEKV <sofie@kvadsheim.priv.no>
2024-03-13 16:37:06 +0100

    Update spill.js

--- spill.js
+++ spill.js
@@ -5,5 +5,21 @@ card.addEventListener("click", function(){
 
 })
 
+const ekteNyhet = document.querySelectorAll("feil")
+
+ekteNyhet.addEventListener("click", function(){
+    ekteNyhet.classList.toggle("farge")
+})
+
+
+const ekteNyhet = document.querySelectorAll("feil")
+for (const feil of ekteNyhet){
+    feil.addEventListener("click", function(evt){
+        const rist = evt.target //får tak i elementet som ble trykket på
+        rist.classList.toggle("rist")
+
+    }
+    )
+}
 
 const rist= document.querySelector
