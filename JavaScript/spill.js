3a6c178427d7b61c19235eded25ddf396523309f
SOFIEKV <sofie@kvadsheim.priv.no>
2024-03-24 12:43:27 +0100

    spill

--- JavaScript/spill.js
+++ JavaScript/spill.js
@@ -1,53 +1,53 @@
-const popup = document.querySelector(".intro");
+const popup = document.querySelector(".intro")
 window.addEventListener("load", function () {
-  popup.classList.add("aktiv");
-});
+  popup.classList.add("aktiv")
+})
 
-const lukk = document.getElementById("lukkKnapp");
+const lukk = document.getElementById("lukkKnapp")
 lukk.addEventListener("click", function () {
   if (popup.classList.contains("aktiv")) {
-    popup.classList.remove("aktiv");
+    popup.classList.remove("aktiv")
   }
-});
+})
 
-const start = document.getElementById("startSpill");
+const start = document.getElementById("startSpill")
 start.addEventListener("click", function () {
   if (popup.classList.contains("aktiv")) {
-    popup.classList.remove("aktiv");
+    popup.classList.remove("aktiv")
   }
-});
+})
 
-const riktig = document.querySelectorAll(".riktig");
+const riktig = document.querySelectorAll(".riktig")
 for (const kortSnud of riktig) {
   kortSnud.addEventListener("click", function () {
-    kortSnud.classList.add("snudd");
-  });
+    kortSnud.classList.add("snudd")
+  })
 }
 
-const feil = document.querySelectorAll(".feil");
+const feil = document.querySelectorAll(".feil")
 for (const shake of feil)
   shake.addEventListener("click", function () {
-    shake.classList.add("shake");
+    shake.classList.add("shake")
     setTimeout(function () {
-      shake.classList.remove("shake");
-    }, 1000);
-  });
+      shake.classList.remove("shake")
+    }, 1000)
+  })
 
 
-let i = 3;
+let i = 2;
 for (const liv of feil) {
   liv.addEventListener("click", function () {
     i--;
-    console.log(i);
+    console.log(i)
 
     if (i === 0) {
-      const melding = document.querySelector(".gameOver");
-      melding.classList.add("paa");
+      const melding = document.querySelector(".gameOver")
+      melding.classList.add("paa")
     }
-  });
+  })
 }
 
-const duVant = document.querySelector(".bak");
+const duVant = document.querySelector(".bak")
 duVant.addEventListener("click", function () {
   window.open("instagram.html", "_parent")
 });
